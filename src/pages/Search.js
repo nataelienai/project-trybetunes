fcc3cb5afab678f9cc4162b00895644fc90fec71
Natã Elienai <nata.elienai@gmail.com>
2021-11-15 17:02:57 -0300

    feat: add a search bar to search for artists

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,45 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artist: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target: { value } }) {
+    this.setState({
+      artist: value,
+    });
+  }
+
   render() {
+    const { artist } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="search"
+            name="artist"
+            value={ artist }
+            onChange={ this.handleInputChange }
+            placeholder="Nome do artista"
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            disabled={ artist.length < 2 }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
