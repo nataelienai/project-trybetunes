b960ad771d607d35a55eacee463f0b90c013b800
Natã Elienai <nata.elienai@gmail.com>
2021-11-16 16:35:59 -0300

    refactor: make username fetch to happen when Header just mounted

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,6 @@ class Header extends React.Component {
   constructor() {
     super();
 
-    this.isComponentMounted = false;
-
     this.state = {
       userName: '',
       isLoading: true,
@@ -18,27 +16,20 @@ class Header extends React.Component {
   }
 
   componentDidMount() {
-    this.isComponentMounted = true;
-  }
-
-  componentWillUnmount() {
-    this.isComponentMounted = false;
+    this.fetchUserName();
   }
 
   async fetchUserName() {
     const user = await getUser();
 
-    if (this.isComponentMounted) {
-      this.setState({
-        userName: user.name,
-        isLoading: false,
-      });
-    }
+    this.setState({
+      userName: user.name,
+      isLoading: false,
+    });
   }
 
   render() {
     const { userName, isLoading } = this.state;
-    this.fetchUserName();
 
     return (
       <header data-testid="header-component">
