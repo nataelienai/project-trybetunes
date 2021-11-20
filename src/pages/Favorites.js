0d1b43cd481d73f6b45a0ecd37abdbc61859675c
Natã Elienai <nata.elienai@gmail.com>
2021-11-20 16:11:01 -0300

    feat: implement favorite songs page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import Loading from '../components/Loading';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      favoriteSongs: [],
+      isLoading: true,
+    };
+
+    this.fetchFavoriteSongs = this.fetchFavoriteSongs.bind(this);
+    this.removeFavoriteSong = this.removeFavoriteSong.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchFavoriteSongs();
+  }
+
+  async fetchFavoriteSongs() {
+    this.setState({
+      favoriteSongs: await getFavoriteSongs(),
+      isLoading: false,
+    });
+  }
+
+  removeFavoriteSong(song) {
+    this.setState({ isLoading: true }, async () => {
+      await removeSong(song);
+      this.fetchFavoriteSongs();
+    });
+  }
+
   render() {
+    const { favoriteSongs, isLoading } = this.state;
+
     return (
       <div data-testid="page-favorites">
         <Header />
+        { isLoading ? <Loading /> : (
+          favoriteSongs.map((song) => (
+            <MusicCard
+              key={ song.trackId }
+              song={ song }
+              isFavorite
+              handleInputChange={ () => this.removeFavoriteSong(song) }
+            />
+          ))
+        )}
       </div>
     );
   }
