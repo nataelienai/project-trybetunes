e7a9c48a8b235c298d1de613345686f0c8970947
Natã Elienai <nata.elienai@gmail.com>
2021-11-20 16:29:21 -0300

    feat: add a profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: {},
+      isLoading: true,
+    };
+
+    this.fetchUser = this.fetchUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  async fetchUser() {
+    this.setState({
+      user: await getUser(),
+      isLoading: false,
+    });
+  }
+
   render() {
+    const { user, isLoading } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
+        { isLoading ? <Loading /> : (
+          <div>
+            <div>
+              <img
+                src={ user.image }
+                alt={ `Perfil de ${user.name}` }
+                data-testid="profile-image"
+              />
+            </div>
+            <div>
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+            <div>
+              <h2>Nome</h2>
+              <p>{ user.name }</p>
+            </div>
+            <div>
+              <h2>E-mail</h2>
+              <p>{ user.email }</p>
+            </div>
+            <div>
+              <h2>Descrição</h2>
+              <p>{ user.description }</p>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
